cdc88f7158d5267cb43251c2d4445774c96efe8f
jack9801 <shishupaljnvr@gmail.com>
2025-09-06 20:47:32 +0530

    add env

--- Rural_Classroom_client/src/firebase.js
+++ Rural_Classroom_client/src/firebase.js
@@ -1,24 +1,8 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCZrT16HJs-3JrbbcSePf-e-EIpehkEPpg",
-//   authDomain: "rural-classroom.firebaseapp.com",
-//   projectId: "rural-classroom",
-//   storageBucket: "rural-classroom.appspot.com",
-//   messagingSenderId: "916425430497",
-//   appId: "1:916425430497:web:ba178206cb6a0f12402ea0",
-//   measurementId: "G-XZSY6TT3GT",
-// };
-const firebaseConfig = {
-  apiKey: "AIzaSyDRbL5v09xA92jeEQOpLa_kDfH6Y8Bx5k0",
-  authDomain: "class-54ed8.firebaseapp.com",
-  projectId: "class-54ed8",
-  storageBucket: "class-54ed8.firebasestorage.app",
-  messagingSenderId: "984103569491",
-  appId: "1:984103569491:web:f0a0d69bda069b14069ea6",
-  measurementId: "G-27NMGEZ1G2"
-};
+
+const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
--- rural_classroom_server/config/firebase.js
+++ rural_classroom_server/config/firebase.js
@@ -1,9 +1,2 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDRbL5v09xA92jeEQOpLa_kDfH6Y8Bx5k0",
-  authDomain: "class-54ed8.firebaseapp.com",
-  projectId: "class-54ed8",
-  storageBucket: "class-54ed8.firebasestorage.app",
-  messagingSenderId: "984103569491",
-  appId: "1:984103569491:web:f0a0d69bda069b14069ea6",
-  measurementId: "G-27NMGEZ1G2"
-};
+const firebaseConfig =process.env.FIREBASE_CONFIG;
+module.exports = firebaseConfig;
